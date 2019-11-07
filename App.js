2905d4d1526fd64c035f3bb5649988b17921e968
alainb-creator <alainbendejac@gmail.com>
2019-11-07 16:53:18 +0100

    enfin

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Test from './Models/test'
+import List from './Models/List'
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <List></List>
+    )
+  }
+}
